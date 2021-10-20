9ccda8c97e9ae98d19b87e07af9ff51bb523077b
shinjun14 <shinjunsakano@gmail.com>
2021-10-20 05:49:39 +0000

    新規登録

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -3,10 +3,12 @@ class Customer::ItemsController < ApplicationController
   def index
     @items = Item.all
     @genres = Genre.all
+    @customer = current_customer
   end
 
   def show
     @item = Item.find( prams[:id] )
+    @customer = current_customer
   end
 
 end
