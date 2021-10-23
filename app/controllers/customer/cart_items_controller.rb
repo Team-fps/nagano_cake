f8c70db98ffb726c4b6a27cbf37b90f612905531
EC2 Default User <ec2-user@ip-172-31-18-92.ec2.internal>
2021-10-23 08:51:26 +0000

    add カート機能

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,15 +1,11 @@
 class Customer::CartItemsController < ApplicationController
   def index
-    @cart_items = current_customer.cart_items
-    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price}
+    @cart_items = CartItem.where(customer_id: current_customer.id)
     @customer = current_customer
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    
-
     @cart_item.save
     redirect_to cart_items_path
   end
@@ -34,7 +30,7 @@ class Customer::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :count)
+    params.require(:cart_item).permit(:customer_id, :item_id, :count)
   end
 
 end
