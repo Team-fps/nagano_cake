91c757630fe7aa00769ae1c3a8fd511133d2a701
kinjito <destiniyfreedom@gmail.com>
2021-10-23 05:15:54 +0000

    退会機能追加

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -5,19 +5,29 @@ class Customer::CustomersController < ApplicationController
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     @customer = current_customer
     @customer.update(customer_params)
     redirect_to customers_path
   end
-  
+
+  def unsubscribe
+    @customer = Customer.find_by(email: params[:email])
+  end
+
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
   private
-  
+
   def customer_params
     params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
   end
