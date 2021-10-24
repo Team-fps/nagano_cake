f65d9d114aa393f9f8c1f9e830892da8e976ade4
shinjun14 <shinjunsakano@gmail.com>
2021-10-24 22:42:12 +0000

    add

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,10 @@ class Order < ApplicationRecord
   
 
   has_many :order_details, dependent: :destroy
+  
+  def sum_of_price
+    item.price * count
+  end
 
   enum payment_method: { クレジットカード: false, 銀行振込: true }
   enum order_status: { 入金待ち: 0, 入金確認: 1, 制作中: 2, 発送準備中: 3, 発送済み: 4 }
