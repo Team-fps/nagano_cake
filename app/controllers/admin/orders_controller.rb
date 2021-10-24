e4e5ee63c42614e2fc09a4b161044a506968d6e6
EC2 Default User <ec2-user@ip-172-31-18-21.ec2.internal>
2021-10-24 04:52:48 +0000

    [Save]order_detail

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,21 +1,23 @@
 class Admin::OrdersController < ApplicationController
+
   def show
     @order = Order.find(params[:id])
+    @ordered_products = @order.order_details
   end
 
-  def update
-    order = Order.find(params[:id])
-    ActiveRecord::Base.transaction do
-      order.update_attributes!(order_params)
-      OrderDetail.multi_update(order_detail_params)
-    end
-    redirect_back(fallback_location: root_path)
-  end
+  # def update
+  #   order = Order.find(params[:id])
+  #   ActiveRecord::Base.transaction do
+  #     order.update_attributes!(order_params)
+  #     OrderDetail.multi_update(order_detail_params)
+  #   end
+  #   redirect_back(fallback_location: root_path)
+  # end
 
-   private
+  private
 
   def order_params
-    params.require(:order).permit(:order_status)
+    params.require(:order).permit(:order_status).merge(order_details: production_status)
   end
 
   def order_detail_params
